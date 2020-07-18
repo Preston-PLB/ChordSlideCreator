2f20b44244be47cd39cdf2c4b125733b7142c0e3
preston <preston@baxterboyz.com>
2020-07-18 10:47:12 -0500

    Functioning Gui

--- chordslidecreator/frontend/src/App.vue
+++ chordslidecreator/frontend/src/App.vue
@@ -91,8 +91,9 @@ export default {
 <style>
   body {
     margin: 0;
-    color: white;
+    color: aliceblue;
     font-family: Ubuntu;
+    background: #555555;
   }
 
   .header{
@@ -106,6 +107,52 @@ export default {
 
   .content {
     display: flex;
+  }
+
+  .content div {
+    /*border: black 2px solid;*/
+  }
+
+  .files{
+    flex: 1;
+    padding: 5px;
+  }
+
+  .fonts{
+    flex: 1;
+    padding: 5px;
+  }
+
+  .fonts select{
+    width: 100%;
+  }
+
+  .settings{
+    flex: 1;
+    padding: 5px;
+  }
+
+  .option{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  .vertical-flex{
+    flex-direction: column;
+  }
+
+
+  .option p {
+    flex: 2;
+    padding: 0 10px 0 0;
+    margin: 0
+  }
+
+  .option input{
+    flex: 1;
+    margin: 0;
   }
 </style>
 
